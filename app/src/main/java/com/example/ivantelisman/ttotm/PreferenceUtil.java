1c310ff0c52de02c87882b55c753d51c0ae9861c
Ivan Telisman <ivan.telisman@datascan.com>
2018-06-29 17:13:41 -0400

    Spinner Setup, need to fix notifications, added dates for the notifications in shared pref

--- app/src/main/java/com/example/ivantelisman/ttotm/PreferenceUtil.java
+++ app/src/main/java/com/example/ivantelisman/ttotm/PreferenceUtil.java
@@ -3,6 +3,9 @@ package com.example.ivantelisman.ttotm;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PreferenceUtil {
     static PreferenceUtil preferenceUtil;
     Context context;
@@ -70,6 +73,37 @@ public class PreferenceUtil {
      */
     public boolean getNotificationHasShown() {
         return sharedPreferences.getBoolean("NOTIFICATION_HAS_SHOWN", false);
+    }
+
+    public void saveNotificationDates(String day0, String day1, String day2, String day3, String day4, String day14) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("DAY_ZERO", day0);
+        editor.putString("DAY_ONE", day1);
+        editor.putString("DAY_TWO", day2);
+        editor.putString("DAY_THREE", day3);
+        editor.putString("DAY_FOUR", day4);
+        editor.putString("DAY_FOURTEEN", day14);
+        editor.commit();
+    }
+
+    /**
+     * Returns the NotificationHasShown as a boolean.
+     */
+    public List<String> getNotificationDates() {
+        String day0 = sharedPreferences.getString("DAY_ZERO", null);
+        String day1 = sharedPreferences.getString("DAY_ONE", null);
+        String day2 = sharedPreferences.getString("DAY_TWO", null);
+        String day3 = sharedPreferences.getString("DAY_THREE", null);
+        String day4 = sharedPreferences.getString("DAY_FOUR", null);
+        String day14 = sharedPreferences.getString("DAY_FOURTEEN", null);
+        List<String> list = new ArrayList<>();
+        list.add(day0);
+        list.add(day1);
+        list.add(day2);
+        list.add(day3);
+        list.add(day4);
+        list.add(day14);
+        return list;
     }
 
 }
--- app/src/main/java/com/example/ivantelisman/ttotm/fragments/MainFragment.java
+++ app/src/main/java/com/example/ivantelisman/ttotm/fragments/MainFragment.java
@@ -11,10 +11,12 @@ import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
-import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,6 +27,7 @@ import com.example.ivantelisman.ttotm.db.AppDatabase;
 import com.example.ivantelisman.ttotm.db.User;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -39,21 +42,23 @@ public class MainFragment extends Fragment {
     private TextView mDateTextView;
     private Button mSubmitButton;
     private Button mContinueButton;
-    private EditText mCycleLength;
     private ImageView mImageViewCalender;
     private CardView mCardView;
+    private Spinner mSpinnerCycleDays;
     private User user;
-    private Calendar mMyCalendar = Calendar.getInstance();
+    private Calendar mDateSelectedCalendar = Calendar.getInstance();
     private Calendar mCurrentDayCalender = Calendar.getInstance();
-    private Calendar mEstimatedDay = Calendar.getInstance();
+    private Calendar mEstimatedDayCalender = Calendar.getInstance();
     private MainActivityViewModel mainActivityViewModel;
     private DatePickerDialog.OnDateSetListener mDate;
     private Date mDateSelected = new Date();
     private Date mEstimatedDate = new Date();
     private View mView;
     private boolean mHasBeenClicked = false;
-    private int mSelectedDayOfTheYear;
+    private int mSelectedDayOfTheYear, mDifferenceInDays, mCycleDays;
     private String mEstimatedCycleStartDate;
+    private String cycleDay;
+    private List<String> cycleDaysList = new ArrayList<>();
 
     public MainFragment() {
         // Required empty public constructor
@@ -67,9 +72,9 @@ public class MainFragment extends Fragment {
         mDateTextView = mView.findViewById(R.id.textViewSelectADate);
         mImageViewCalender = mView.findViewById(R.id.imageViewCalender);
         mContinueButton = mView.findViewById(R.id.continueButton);
-        mCycleLength = mView.findViewById(R.id.cycleLength);
         mSubmitButton = mView.findViewById(R.id.submit);
         mCardView = mView.findViewById(R.id.buttonsAndEditTextCard);
+        mSpinnerCycleDays = mView.findViewById(R.id.spinnerCycleLength);
 
         // Note: Db references should not be in an activity.
         mDb = AppDatabase.getInMemoryDatabase(getContext());
@@ -77,15 +82,17 @@ public class MainFragment extends Fragment {
         // Get a reference to the ViewModel for this screen.
         mainActivityViewModel = ViewModelProviders.of(this).get(MainActivityViewModel.class);
 
+        setUpCycleDaysSpinner();
+
         mDate = new DatePickerDialog.OnDateSetListener() {
 
             @Override
             public void onDateSet(DatePicker view, int year, int monthOfYear,
                                   int dayOfMonth) {
                 // TODO Auto-generated method stub
-                mMyCalendar.set(Calendar.YEAR, year);
-                mMyCalendar.set(Calendar.MONTH, monthOfYear);
-                mMyCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                mDateSelectedCalendar.set(Calendar.YEAR, year);
+                mDateSelectedCalendar.set(Calendar.MONTH, monthOfYear);
+                mDateSelectedCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
                 updateLabel();
             }
         };
@@ -96,44 +103,46 @@ public class MainFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 mHasBeenClicked = true;
-                new DatePickerDialog(getContext(), mDate, mMyCalendar
-                        .get(Calendar.YEAR), mMyCalendar.get(Calendar.MONTH),
-                        mMyCalendar.get(Calendar.DAY_OF_MONTH)).show();
+                new DatePickerDialog(getContext(), mDate, mDateSelectedCalendar
+                        .get(Calendar.YEAR), mDateSelectedCalendar.get(Calendar.MONTH),
+                        mDateSelectedCalendar.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
 
         mSubmitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mMyCalendar.setTime(mDateSelected);
-                mSelectedDayOfTheYear = mMyCalendar.get(Calendar.DAY_OF_YEAR);
-                Calendar calendar = Calendar.getInstance();
-                calendar.set(Calendar.DAY_OF_YEAR, mSelectedDayOfTheYear + Integer.valueOf(mCycleLength.getText().toString().trim()));
-                mEstimatedCycleStartDate = calendar.getTime().toString();
+                mCycleDays = Integer.valueOf(cycleDay);
+                //selected date
+                mDateSelectedCalendar.setTime(mDateSelected);
+                mSelectedDayOfTheYear = mDateSelectedCalendar.get(Calendar.DAY_OF_YEAR);
+                //estimated date
+                mEstimatedDayCalender.setTime(mDateSelected);
+                mEstimatedCycleStartDate = String.valueOf(mDateSelectedCalendar.get(Calendar.DAY_OF_YEAR) + mCycleDays);
+                //convert mEstimatedCycleStartDate day of year to date format
+                mEstimatedDayCalender.set(Calendar.DAY_OF_YEAR, Integer.valueOf(mEstimatedCycleStartDate));
+                mEstimatedDate = mEstimatedDayCalender.getTime();
 
-                //For calender
-                mMyCalendar.setTime(mDateSelected);
-                mEstimatedDay.setTime(mEstimatedDate);
+                mDifferenceInDays = mCurrentDayCalender.get(Calendar.DAY_OF_YEAR) - Integer.valueOf(mEstimatedCycleStartDate);
 
-                if (mCycleLength.getText().toString().trim().isEmpty() || (Integer.valueOf(mCycleLength.getText().toString().trim()) < 24 || Integer.valueOf(mCycleLength.getText().toString().trim()) > 34)) {
-                    mCycleLength.setError("Enter a valid number between 24 and 34");
+                if (cycleDay.equals("0")) {
+                    Toast.makeText(getContext(), "Choose a cycle day amount between 24 and 34", Toast.LENGTH_LONG).show();
                 } else if (!mHasBeenClicked) {
                     Toast.makeText(getContext(), "Select A Date From The Calender", Toast.LENGTH_LONG).show();
                 } else if (mDateSelected.getTime() > System.currentTimeMillis()) {
                     Toast.makeText(getContext(), "Please Select A Past Date From The Calender", Toast.LENGTH_LONG).show();
-                } else if ((mCurrentDayCalender.get(Calendar.DAY_OF_YEAR) - mEstimatedDay.get(Calendar.DAY_OF_YEAR)) >= 0) {
+                } else if (mDifferenceInDays >= 0) {
                     Toast.makeText(getContext(), "That Date Is Out Of Possible Ranges!", Toast.LENGTH_LONG).show();
                 } else{
                     mainActivityViewModel.mDb.userModel().deleteAll();
                     user = new User();
                     user.id = "0";
-                    user.cycleDuration = Integer.valueOf(mCycleLength.getText().toString().trim());
-                    user.differenceInDays = mCurrentDayCalender.get(Calendar.DAY_OF_YEAR) - mEstimatedDay.get(Calendar.DAY_OF_YEAR);
-                    user.estimatedStartDate = mEstimatedCycleStartDate;
+                    user.cycleDuration = Integer.valueOf(cycleDay);
+                    user.differenceInDays = mDifferenceInDays;
+                    user.estimatedStartDate = mEstimatedDate.toString();
                     user.date = mDateSelected.toString();
                     mDb.userModel().insertUser(user);
-                    PreferenceUtil.getInstance(getContext()).saveDifferenceInDays(mCurrentDayCalender.get(Calendar.DAY_OF_YEAR) - mEstimatedDay.get(Calendar.DAY_OF_YEAR));
-                    //Log.d("submitDate: ", String.valueOf(mMyCalendar.get(Calendar.DAY_OF_YEAR) - (mCurrentDay.get(Calendar.DAY_OF_YEAR))));
+                    PreferenceUtil.getInstance(getContext()).saveDifferenceInDays(mDifferenceInDays);
                     getFragmentManager().beginTransaction().replace(R.id.content, new CalanderFragment(), "calender").commit();
                 }
             }
@@ -152,10 +161,10 @@ public class MainFragment extends Fragment {
     private void updateLabel() {
         String myFormat = "MM/dd/yy"; //In which you need put here
         SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
-        mDateSelected = mMyCalendar.getTime();
+        mDateSelected = mDateSelectedCalendar.getTime();
         //ui changes
         mSubmitButton.setVisibility(View.VISIBLE);
-        mCycleLength.setVisibility(View.VISIBLE);
+        mSpinnerCycleDays.setVisibility(View.VISIBLE);
         mCardView.setVisibility(View.VISIBLE);
         mDateTextView.setText("DATE SELECTED:\n" + sdf.format(mDateSelected));
     }
@@ -165,6 +174,7 @@ public class MainFragment extends Fragment {
             String myFormat = "MM/dd/yy"; //In which you need put here
             SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
             SimpleDateFormat dateFormat = new SimpleDateFormat("EE MMM dd HH:mm:ss z yyyy", Locale.ENGLISH);
+
             @Override
             public void onChanged(@NonNull final List<User> users) {
                 try{
@@ -173,10 +183,9 @@ public class MainFragment extends Fragment {
                     //ui changes
                     mDateTextView.setText("DATE THE LAST PERIOD BEGAN:\n" + /*users.get(0).date*/sdf.format(mDateSelected) + "\n\nESTIMATED START DATE OF THE NEXT PERIOD:\n" + sdf.format(mEstimatedDate) + "\n\nIF THE LATEST PERIOD HAS COME CLICK THE CALENDER AND SELECT THE DATE IT BEGAN");
                     mSubmitButton.setVisibility(View.GONE);
-                    mCycleLength.setVisibility(View.GONE);
+                    mSpinnerCycleDays.setVisibility(View.GONE);
                     mCardView.setVisibility(View.GONE);
-                }
-                catch (Exception e){
+                } catch (Exception e){
                     //ui changes
                     mDateTextView.setText("CLICK THE CALENDER AND SELECT THE DATE THE LAST PERIOD BEGAN");
                     mContinueButton.setVisibility(View.GONE);
@@ -186,5 +195,57 @@ public class MainFragment extends Fragment {
         });
     }
 
-}
+    public void setUpCycleDaysSpinner() {
+        cycleDaysList.add("Cycle Length");
+        cycleDaysList.add("24");
+        cycleDaysList.add("25");
+        cycleDaysList.add("26");
+        cycleDaysList.add("27");
+        cycleDaysList.add("28");
+        cycleDaysList.add("29");
+        cycleDaysList.add("30");
+        cycleDaysList.add("31");
+        cycleDaysList.add("32");
+        cycleDaysList.add("33");
+        cycleDaysList.add("34");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getContext(), R.layout.day_spinner_item, cycleDaysList);
+        mSpinnerCycleDays.setAdapter(arrayAdapter);
+        mSpinnerCycleDays.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (i == 0) {
+                    cycleDay = "0";
+                } else if (i == 1) {
+                    cycleDay = "24";
+                } else if (i == 2) {
+                    cycleDay = "25";
+                } else if (i == 3) {
+                    cycleDay = "26";
+                } else if (i == 4) {
+                    cycleDay = "27";
+                } else if (i == 5) {
+                    cycleDay = "28";
+                } else if (i == 6) {
+                    cycleDay = "29";
+                } else if (i == 7) {
+                    cycleDay = "30";
+                } else if (i == 8) {
+                    cycleDay = "31";
+                } else if (i == 9) {
+                    cycleDay = "32";
+                } else if (i == 10) {
+                    cycleDay = "33";
+                } else if (i == 11) {
+                    cycleDay = "34";
+                }
 
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                cycleDay = "0";
+            }
+        });
+    }
+
+}
